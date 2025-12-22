46069615ce20c3f4e9dd34d5da64e5115289c223
MysteriousOyster <leifwilliambarton@gmail.com>
2025-12-22 14:31:53 -0700

    Refactor code structure for improved readability and maintainability

---
+++ pi_run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root or with sudo!" >&2
+    exit 1
+fi
+
+chmod +x ./target/aarch64-unknown-linux-gnu/release/indra
+./target/aarch64-unknown-linux-gnu/release/indra
